c7507abeec7222feff9bf572cd619c61933d0def
craigh210 <craighunt210@gmail.com>
2021-04-01 18:21:42 -0400

    Initial commit

---
+++ src/test/java/edu/mc/spring21jwd402/Spring21jwd402ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.spring21jwd402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring21jwd402ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
